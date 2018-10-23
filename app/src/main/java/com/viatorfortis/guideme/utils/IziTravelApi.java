7c681b051dd14a8f7cc8f437ae7690ab5d70678b
Антон <strider-13@mail.ru>
2018-10-23 23:29:32 +1000

    feat: add getMuseumAndTourByRegionJson()

--- app/src/main/java/com/viatorfortis/guideme/utils/IziTravelApi.java
+++ app/src/main/java/com/viatorfortis/guideme/utils/IziTravelApi.java
@@ -140,4 +140,10 @@ public class IziTravelApi {
         return getHttpResponse(context, url);
     }
 
+    public static String getMuseumAndTourByRegionJson(Context context, RegionType regionType, String regionUuid, String languages)
+            throws IOException {
+        URL url = buildGetMuseumAndTourByRegionUrl(context, regionType, regionUuid, languages);
+        return getHttpResponse(context, url);
+    }
+
 }
