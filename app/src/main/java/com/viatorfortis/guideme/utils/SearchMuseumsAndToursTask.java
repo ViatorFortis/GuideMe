f5ea0043ca57e683b6545b3857b44cb262b063af
Антон <strider-13@mail.ru>
2018-10-31 20:40:49 +1000

    feat: add SearchMuseumsAndToursTask class

---
+++ app/src/main/java/com/viatorfortis/guideme/utils/SearchMuseumsAndToursTask.java
@@ -0,0 +1,36 @@
+package com.viatorfortis.guideme.utils;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class SearchMuseumsAndToursTask extends AsyncTask <String, Void, String> {
+
+    private final String Tag = this.getClass().getSimpleName();
+    private Context context;
+
+    public SearchMuseumsAndToursTask(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... searchParameters) {
+        try {
+            return IziTravelApi.getMuseumAndTourListBySearchJson(context, searchParameters[0], searchParameters[1]);
+        } catch (IOException e) {
+            Log.e(Tag, e.getMessage() );
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        Toast.makeText(context, "getMuseumAndTourListBySearchJson retuurns " + result, Toast.LENGTH_LONG).show();
+    }
+}
