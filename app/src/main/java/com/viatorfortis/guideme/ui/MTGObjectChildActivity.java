6a740f37108caf16dc89aab2a0a6a2b12984db83
Антон <strider-13@mail.ru>
2019-03-01 16:45:15 +1000

    feat: add custom view for ActionBar, Picasso for image loading and displaying, set navigation up

--- app/src/main/java/com/viatorfortis/guideme/ui/MTGObjectChildActivity.java
+++ app/src/main/java/com/viatorfortis/guideme/ui/MTGObjectChildActivity.java
@@ -1,12 +1,20 @@
 package com.viatorfortis.guideme.ui;
 
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
 import com.viatorfortis.guideme.R;
 import com.viatorfortis.guideme.model.Child;
+
+import static com.viatorfortis.guideme.utils.IziTravelApi.buildStoryImageUri;
 
 public class MTGObjectChildActivity extends AppCompatActivity {
 
@@ -26,7 +34,47 @@ public class MTGObjectChildActivity extends AppCompatActivity {
             return;
         }
 
+//        Toolbar appBar = findViewById(R.id.tb_mtgobject_child);
+//        setSupportActionBar(appBar);
+//        if (getSupportActionBar() != null) {
+//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//            getSupportActionBar().setDisplayShowHomeEnabled(true);
+//            getSupportActionBar().setTitle(mChild.getType() );
+//        }
+
+        setSupportActionBar((Toolbar) findViewById(R.id.tb_mtgobject_child) );
+
+        if (getSupportActionBar() != null) {
+            ActionBar actionBar = getSupportActionBar();
+            //actionBar.setDisplayOptions();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayShowCustomEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setCustomView(R.layout.actionbar_title);
+            ( (TextView) findViewById(R.id.ab_title) ).setText(mChild.getTitle() );
+        }
+
+
         ( (TextView) findViewById(R.id.tv_title) ).setText(mChild.getTitle() );
         ( (TextView) findViewById(R.id.tv_desc) ).setText(mChild.getDesc() );
+
+        ImageView pointImageView = findViewById(R.id.iv_point_image);
+        Picasso.with(this)
+                .load(buildStoryImageUri(mChild.getContentProvider().getUuid(), mChild.getImages().get(0).getUuid(), "800x600") )
+                .into(pointImageView);
+
+        ( (TextView) findViewById(R.id.tv_content_provider) ).setText(mChild.getContentProvider().getName() );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
