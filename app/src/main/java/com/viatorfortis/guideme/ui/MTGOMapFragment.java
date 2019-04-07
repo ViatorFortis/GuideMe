0b23c091c6cf975768322f523d5e582ad07d6fa5
Антон <strider-13@mail.ru>
2019-04-07 21:53:53 +1000

    feat: add children locations to MapView

--- app/src/main/java/com/viatorfortis/guideme/ui/MTGOMapFragment.java
+++ app/src/main/java/com/viatorfortis/guideme/ui/MTGOMapFragment.java
@@ -1,6 +1,5 @@
 package com.viatorfortis.guideme.ui;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,19 +8,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.viatorfortis.guideme.R;
+import com.viatorfortis.guideme.model.Child;
 import com.viatorfortis.guideme.model.FullFormMTGObject;
-import com.viatorfortis.guideme.model.Map;
+import com.viatorfortis.guideme.model.Location;
+import com.viatorfortis.guideme.model.TriggerZone;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MTGOMapFragment extends Fragment {
 
@@ -65,7 +69,7 @@ public class MTGOMapFragment extends Fragment {
         //mMapView.onResume();
 
         try {
-            MapsInitializer.initialize(getActivity().getApplicationContext() );
+            MapsInitializer.initialize(getActivity().getApplicationContext());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -77,6 +81,13 @@ public class MTGOMapFragment extends Fragment {
 
                 MoveCameraToMTGObjectBounds();
                 AddRouteToMap();
+
+                List<Location> locationList = new ArrayList<Location>();
+                List<TriggerZone> triggerZoneList = new ArrayList<TriggerZone>();
+
+                getChildrenInfo(locationList, triggerZoneList);
+
+                addChildrenLocationsToMap(locationList);
             }
         });
 
@@ -84,7 +95,7 @@ public class MTGOMapFragment extends Fragment {
     }
 
     private void MoveCameraToMTGObjectBounds() {
-        String [] mapBoundsArray = mFullFormMTGObject.getMap().getBounds().split(",");
+        String[] mapBoundsArray = mFullFormMTGObject.getMap().getBounds().split(",");
 
         LatLng swLatLng = new LatLng(
                 Float.parseFloat(mapBoundsArray[0]),
@@ -98,7 +109,7 @@ public class MTGOMapFragment extends Fragment {
 
         LatLngBounds mapBounds = new LatLngBounds(swLatLng, neLatLng);
 
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(mapBounds, 0) );
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(mapBounds, 0));
     }
 
     private void AddRouteToMap() {
@@ -106,16 +117,47 @@ public class MTGOMapFragment extends Fragment {
         PolylineOptions polylineOptions = new PolylineOptions();
 
         for (String point : routeString.split(";")) {
-            String [] pointPosition = point.split(",");
+            String[] pointPosition = point.split(",");
             polylineOptions.add(new LatLng(
                     Float.parseFloat(pointPosition[0]),
-                    Float.parseFloat(pointPosition[1]) ) );
+                    Float.parseFloat(pointPosition[1])));
         }
 
-        polylineOptions.color(getResources().getColor(R.color.colorAccent) )
+        polylineOptions.color(getResources().getColor(R.color.colorAccent))
                 .width(5);
         mGoogleMap.addPolyline(polylineOptions);
     }
+
+    private void getChildrenInfo(List<Location> locationList, List<TriggerZone> triggerZoneList) {
+
+        List<Child> ChildList = mFullFormMTGObject.getContentList().get(0).getChildren();
+
+        for (Child child : ChildList) {
+            if (!child.getType().equals(getString(R.string.mtgo_type_story_navigation))) {
+                if (child.getLocation() != null) {
+                    locationList.add(child.getLocation());
+                }
+
+                if (child.getTriggerZones() != null) {
+                    for (TriggerZone triggerZone : child.getTriggerZones()) {
+                        triggerZoneList.add(triggerZone);
+                    }
+                }
+            }
+        }
+    }
+
+    public void addChildrenLocationsToMap(List<Location> locationList) {
+        for (Location location : locationList) {
+            float lat = location.getLatitude().floatValue();
+            float lng = location.getLongitude().floatValue();
+
+            MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(lat, lng));
+            mGoogleMap.addMarker(markerOptions);
+        }
+    }
+
+
 
     @Override
     public void onResume() {
